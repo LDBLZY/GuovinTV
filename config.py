210224c13b95f7cc6c85b085d83bba0e02b47c26
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-05 09:58:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,26 +6,26 @@ favorite_list = [
     "湖北经视",
     "CCTV13",
 ]
-open_online_search = True
+open_online_search = False
 favorite_page_num = 5
 default_page_num = 3
 urls_limit = 15
 open_keep_all = False
-open_sort = True
+open_sort = False
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
 subscribe_urls = [
-    "https://m3u.ibert.me/txt/fmml_dv6.txt",
-    "https://m3u.ibert.me/txt/o_cn.txt",
-    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://m3u.ibert.me/txt/fmml_ipv6.txt",
+    "https://m3u.ibert.me/txt/ycl_iptv.txt",
+    "https://m3u.ibert.me/txt/m_iptv.txt",
 ]
-open_multicast = True
-region_list = ["all"]
+open_multicast = False
+region_list = ["湖北"]
 open_proxy = True
-open_driver = False
-open_use_old_result = True
+open_driver = True
+open_use_old_result = False
